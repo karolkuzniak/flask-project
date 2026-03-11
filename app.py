a72e0cb1d2852e858c8d3ab4f5ec9e3cd222f3b8
kkuzn <karolkuzniak1@gmail.com>
2026-03-11 14:13:08 +0100

    Add monitoring stack: Flask + Prometheus + Grafana

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from statsd import StatsClient
 import logging
 from flask import Flask
+from prometheus_client import Counter, generate_latest
+from flask import Response
 
 #logging configuration
 logging.basicConfig(
@@ -15,27 +17,48 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
-#klient statsd
+#client statsd
 statsd = StatsClient(host='localhost', port=8125, prefix='flask_app')
+
+#Prometheus metrics
+REQUEST_COUNT = Counter(
+    "flask_requests_total",
+    "Total number of HTTP requests"
+)
+
+ERROR_COUNT = Counter(
+    "flask_errors_total",
+    "Total number of application errors"
+)
+
+#counting all requests
+@app.before_request
+def before_request():
+    REQUEST_COUNT.inc()
+
+#endpoint for Prometheus
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype="text/plain")
 
 @app.route('/')
 def home():
-    logger.info("Wywołano endpoint /")
+    logger.info("output endpoint /")
     return "Flask works :)"
 
 @app.route('/debug')
 def debug():
-    logger.info("Debug endpoint")
+    logger.debug("Debug endpoint")
     return "Debug log"
 
 @app.route('/warning')
 def warning():
-    logger.info("Warning endpoint")
+    logger.warning("Warning endpoint")
     return "Warning log"
 
 @app.route('/error')
 def error():
-    logger.info("error endpoint")
+    logger.error("error endpoint")
     return "Error log"
 
 @app.route('/simulate-error')
@@ -43,6 +66,7 @@ def simulate_error():
     logger.info("Endpoint /fail został wywołany")
     raise Exception("Testowy wyjątek")
 
+#handling exceptions
 @app.errorhandler(Exception)
 def handle_exception(error):
     logger.exception("Wystąpił wyjątek w aplikacji")
@@ -50,13 +74,15 @@ def handle_exception(error):
     #wysyłanie metryki do StatsD
     statsd.incr("exception.total")
 
-    return {
-        "error": str(error)
-    }, 500
+    #Prometheus metrics
+    ERROR_COUNT.inc()
+
+    return {"error": str(error)}, 500
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     logger.info("Starting Flask app")
-    app.run(debug=True)
-
-
-statsd = StatsClient(host='localhost', port=8125, prefix='flask_app')
+    app.run(host="0.0.0.0", port=5000)
